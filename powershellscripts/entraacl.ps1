1f1db1e5c96d64590f5c443ecb8c8c20747a16ef
smokethis <64678625+smokethis@users.noreply.github.com>
2024-09-09 14:24:14 +0100

    kfc

---
+++ powershellscripts/entraacl.ps1
@@ -0,0 +1,22 @@
+# Get Parameters from 
+param(
+    [Parameter(Mandatory)]
+    [string]$sqlServerName
+)
+# Connect to Graph
+Connect-MgGraph
+# Create New Group for Directory Readers
+$group = New-MgGroup -DisplayName "DirectoryReaderGroup" -Description "Directory Reader Group" -SecurityEnabled:$true -IsAssignableToRole:$true -MailEnabled:$false -MailNickname "DirRead"
+$group
+# Displays the Directory Readers role information
+$roleDefinition = Get-MgRoleManagementDirectoryRoleDefinition -Filter "DisplayName eq 'Directory Readers'"
+$roleDefinition
+# Assigns the Directory Readers role to the group
+$roleAssignment = New-MgRoleManagementDirectoryRoleAssignment -DirectoryScopeId '/' -RoleDefinitionId $roleDefinition.Id -PrincipalId $group.Id
+$roleAssignment
+# Returns the service principal of your Azure SQL resource
+$managedIdentity = Get-MgServicePrincipal -Filter "displayName eq '$sqlServerName'"
+$managedIdentity
+# Adds the service principal to the group
+New-MgGroupMember -GroupId $group.Id -DirectoryObjectId $managedIdentity.Id
+Get-MgGroupMember -GroupId $group.Id -Filter "Id eq '$($managedIdentity.Id)'"
