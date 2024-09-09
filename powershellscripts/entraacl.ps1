61226090709ab20b7298db9734c844974d3f7010
smokethis <64678625+smokethis@users.noreply.github.com>
2024-09-09 17:03:47 +0100

    pis off

--- powershellscripts/entraacl.ps1
+++ powershellscripts/entraacl.ps1
@@ -3,7 +3,7 @@ param(
     [Parameter(Mandatory)]
     [string]$sqlServerName
 )
-Install-Module -name JWTDetails
+Install-Module -name JWTDetails -Force
 $token = az account get-access-token --scope https://graph.microsoft.com/.default --query accessToken --output tsv
 $jwtTokenDetails = get-JWTDetails ($token)
 $jwtTokenDetails
