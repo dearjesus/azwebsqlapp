55e7a4dfe8a2d9551ac1848db3a7ae89ffe0dd0d
smokethis <64678625+smokethis@users.noreply.github.com>
2024-09-09 11:49:37 +0100

    script?

--- sqlscripts/acl.sql
+++ sqlscripts/acl.sql
@@ -1,4 +1,4 @@
 -- Crete user for webapp managed identity and grant rights
-CREATE USER [$(WebAppName)] FROM EXTERNAL PROVIDER
-ALTER ROLE db_datareader ADD MEMBER [$(WebAppName)]
-ALTER ROLE db_datawriter ADD MEMBER [$(WebAppName)]
+CREATE USER [${{ steps.deploy.outputs.webSiteName }}] FROM EXTERNAL PROVIDER
+ALTER ROLE db_datareader ADD MEMBER [${{ steps.deploy.outputs.webSiteName }}]
+ALTER ROLE db_datawriter ADD MEMBER [${{ steps.deploy.outputs.webSiteName }}]
