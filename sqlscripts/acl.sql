3dfc36918adc56e517fdd45be958d74214048b22
smokethis <64678625+smokethis@users.noreply.github.com>
2024-09-09 11:55:42 +0100

    Fix sql args

--- sqlscripts/acl.sql
+++ sqlscripts/acl.sql
@@ -1,4 +1,4 @@
 -- Crete user for webapp managed identity and grant rights
-CREATE USER [${{ steps.deploy.outputs.webSiteName }}] FROM EXTERNAL PROVIDER
-ALTER ROLE db_datareader ADD MEMBER [${{ steps.deploy.outputs.webSiteName }}]
-ALTER ROLE db_datawriter ADD MEMBER [${{ steps.deploy.outputs.webSiteName }}]
+CREATE USER [$(webSiteName)] FROM EXTERNAL PROVIDER
+ALTER ROLE db_datareader ADD MEMBER [$(webSiteName)]
+ALTER ROLE db_datawriter ADD MEMBER [$(webSiteName)]
