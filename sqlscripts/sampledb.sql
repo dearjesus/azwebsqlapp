c00d82d5d6c8e4e2f693eaa13a489fe07e6fdd5d
smokethis <64678625+smokethis@users.noreply.github.com>
2024-09-10 13:37:29 +0100

    stupid sql script

--- sqlscripts/sampledb.sql
+++ sqlscripts/sampledb.sql
@@ -1,12 +1,4 @@
--- Step 1: Create the Database
-CREATE DATABASE SampleRLSDB;
-GO
-
--- Use the newly created database
-USE SampleRLSDB;
-GO
-
--- Step 2: Create the Customer Table
+-- Step 1: Create the Customer Table
 CREATE TABLE Customers (
     CustomerID INT PRIMARY KEY,
     CustomerName NVARCHAR(100),
@@ -14,7 +6,7 @@ CREATE TABLE Customers (
 );
 GO
 
--- Step 3: Insert Sample Data
+-- Step 2: Insert Sample Data
 INSERT INTO Customers (CustomerID, CustomerName, UPN)
 VALUES (1, 'Customer A', $(user1upn)),
        (2, 'Customer B', $(user1upn)),
@@ -22,7 +14,7 @@ VALUES (1, 'Customer A', $(user1upn)),
        (4, 'Customer D', $(user2upn));
 GO
 
--- Step 4: Create a Filter Function for RLS
+-- Step 3: Create a Filter Function for RLS
 CREATE FUNCTION dbo.fn_securitypredicate(@UserUPN AS NVARCHAR(255))
 RETURNS TABLE
 WITH SCHEMABINDING
@@ -31,7 +23,7 @@ RETURN SELECT 1 AS Result
     WHERE @UserUPN = SESSION_CONTEXT(N'UserUPN'); -- Compare row UPN with session UPN
 GO
 
--- Step 5: Create a Security Policy for Row-Level Security
+-- Step 4: Create a Security Policy for Row-Level Security
 CREATE SECURITY POLICY CustomerSecurityPolicy
     ADD FILTER PREDICATE dbo.fn_securitypredicate(UPN)
     ON dbo.Customers
