5b3417ab21026a118e8bce92bd2a22064f09417b
smokethis <64678625+smokethis@users.noreply.github.com>
2024-09-10 11:35:25 +0100

    Updates to create table + data

--- web-app/app.py
+++ web-app/app.py
@@ -6,38 +6,62 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-class Person(BaseModel):
-    first_name: str
-    last_name: Union[str, None] = None
+class Sale(BaseModel):
+    OrderId: int
+    AppUserId: int
+    Product: str
+    Qty: int
     
 connection_string = os.environ["AZURE_SQL_CONNECTIONSTRING"]
 
 app = FastAPI()
 
+### MUST VISIT ROOT URL FIRST TO CREATE DATABASE, THANKS MICROSOFT ###
 @app.get("/")
 def root():
-    print("Root of Person API")
-    try:
-        conn = get_conn()
-        cursor = conn.cursor()
+    print("Root of Sales API")
+    # try:
+    #     conn = get_conn()
+    #     cursor = conn.cursor()
 
-        # Table should be created ahead of time in production app.
-        cursor.execute("""
-            CREATE TABLE Persons (
-                ID int NOT NULL PRIMARY KEY IDENTITY,
-                FirstName varchar(255),
-                LastName varchar(255)
-            );
-        """)
+    #     # Table should be created ahead of time in production app.
+    #     cursor.execute("""
+    #         CREATE TABLE Sales (
+    #             OrderId int,
+    #             AppUserId int,
+    #             Product varchar(10),
+    #             Qty int
+    #         );
+    #     """)
 
-        conn.commit()
-    except Exception as e:
-        # Table may already exist
-        print(e)
-    return "Person API"
+    #     conn.commit()
+    # except Exception as e:
+    #     # Table may already exist
+    #     print(e)
+    # try:
+    #     conn = get_conn()
+    #     cursor = conn.cursor()
+
+    #     # Table should be created ahead of time in production app.
+    #     cursor.execute("""
+    #         INSERT Sales VALUES
+    #             (1, 1, 'Valve', 5),
+    #             (2, 1, 'Wheel', 2),
+    #             (3, 1, 'Valve', 4),
+    #             (4, 2, 'Bracket', 2),
+    #             (5, 2, 'Wheel', 5),
+    #             (6, 2, 'Seat', 5);
+    #     """)
+
+    #     conn.commit()
+    # except Exception as e:
+    #     # Rows may already exist
+    #     print(e)
+    
+    return "Sales API"
 
 @app.get("/all")
-def get_persons():
+def sales():
     rows = []
     with get_conn() as conn:
         cursor = conn.cursor()
@@ -49,7 +73,7 @@ def get_persons():
     return rows
 
 @app.get("/person/{person_id}")
-def get_person(person_id: int):
+def get_sale(person_id: int):
     with get_conn() as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM Persons WHERE ID = ?", person_id)
@@ -58,7 +82,7 @@ def get_person(person_id: int):
         return f"{row.ID}, {row.FirstName}, {row.LastName}"
 
 @app.post("/person")
-def create_person(item: Person):
+def create_person(item: Sale):
     with get_conn() as conn:
         cursor = conn.cursor()
         cursor.execute(f"INSERT INTO Persons (FirstName, LastName) VALUES (?, ?)", item.first_name, item.last_name)
