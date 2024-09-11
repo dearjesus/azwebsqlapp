30affb0cd493d8be9b1adce73357f83f47e24b7a
smokethis <64678625+smokethis@users.noreply.github.com>
2024-09-11 09:42:49 +0100

    revert to sql sample

--- main.py
+++ main.py
@@ -1,33 +1,75 @@
-from fastapi import FastAPI, Form, Request, status
-from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import uvicorn
+import os
+import pyodbc, struct
+from azure import identity
 
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Person(BaseModel):
+    first_name: str
+    last_name: Union[str, None] = None
+    
+connection_string = os.environ["AZURE_SQL_CONNECTIONSTRING"]
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
-    print('Request for index page received')
-    return templates.TemplateResponse('index.html', {"request": request})
+@app.get("/")
+def root():
+    print("Root of Person API")
+    try:
+        conn = get_conn()
+        cursor = conn.cursor()
 
-@app.get('/favicon.ico')
-async def favicon():
-    file_name = 'favicon.ico'
-    file_path = './static/' + file_name
-    return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
+        # Table should be created ahead of time in production app.
+        cursor.execute("""
+            CREATE TABLE Persons (
+                ID int NOT NULL PRIMARY KEY IDENTITY,
+                FirstName varchar(255),
+                LastName varchar(255)
+            );
+        """)
 
-@app.post('/hello', response_class=HTMLResponse)
-async def hello(request: Request, name: str = Form(...)):
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
+        conn.commit()
+    except Exception as e:
+        # Table may already exist
+        print(e)
+    return "Person API"
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000)
+@app.get("/all")
+def get_persons():
+    rows = []
+    with get_conn() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Persons")
+
+        for row in cursor.fetchall():
+            print(row.FirstName, row.LastName)
+            rows.append(f"{row.ID}, {row.FirstName}, {row.LastName}")
+    return rows
+
+@app.get("/person/{person_id}")
+def get_person(person_id: int):
+    with get_conn() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Persons WHERE ID = ?", person_id)
+
+        row = cursor.fetchone()
+        return f"{row.ID}, {row.FirstName}, {row.LastName}"
+
+@app.post("/person")
+def create_person(item: Person):
+    with get_conn() as conn:
+        cursor = conn.cursor()
+        cursor.execute(f"INSERT INTO Persons (FirstName, LastName) VALUES (?, ?)", item.first_name, item.last_name)
+        conn.commit()
+
+    return item
+
+def get_conn():
+    credential = identity.DefaultAzureCredential(exclude_interactive_browser_credential=False)
+    token_bytes = credential.get_token("https://database.windows.net/.default").token.encode("UTF-16-LE")
+    token_struct = struct.pack(f'<I{len(token_bytes)}s', len(token_bytes), token_bytes)
+    SQL_COPT_SS_ACCESS_TOKEN = 1256  # This connection option is defined by microsoft in msodbcsql.h
+    conn = pyodbc.connect(connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})
+    return conn
